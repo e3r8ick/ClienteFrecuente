347a4ac63080bb43efbdde89400639669288ad95
Erick Cordero Rojas <eguicoro2@gmail.com>
2017-12-15 10:43:15 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+
+<LINK REL=StyleSheet HREF="css/login.css" TYPE="text/css">
+
+<form class="login">
+    <h1 class="login-title">Cliente Frecuente</h1>
+    <input type="text" class="login-input" placeholder="Cod Cliente" autofocus>
+    <input type="password" class="login-input" placeholder="Contraseña">
+    <input type="submit" value="Ingresar" class="login-button">
+  <p class="login-lost"><a href="php/registro.php">¿No tiene cuenta? Registrar</a></p>
+  </form>
---
+++ php/registro.php
@@ -0,0 +1,9 @@
+<LINK REL=StyleSheet HREF="../css/login.css" TYPE="text/css">
+
+<form class="login">
+    <h1 class="login-title">Cliente Frecuente</h1>
+    <input type="text" class="login-input" placeholder="Cod Cliente" autofocus>
+    <input type="password" class="login-input" placeholder="Contraseña">
+    <input type="password" class="login-input" placeholder="Confirmar Contraseña">
+    <input type="submit" value="Registrar" class="login-button">
+  </form>
