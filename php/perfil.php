404b269c43c5338dd79bf76f5d70945961a6ba42
Erick Cordero Rojas <eguicoro2@gmail.com>
2017-12-15 16:08:50 -0600

    Menu del perfil

--- php/perfil.php
+++ php/perfil.php
@@ -1,6 +1,36 @@
-<LINK REL=StyleSheet HREF="../css/login.css" TYPE="text/css">
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
 
-<nav>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="../css/perfil.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="../css/font-awesome.css" >
+
+     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <script src="http://css3-mediaqueries-js.googlecode.com/files/css3-mediaqueries.js"></script>
+    <![endif]-->
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript" ></script>
+    <script src="../js/perfil.js" type="text/javascript"></script>
+
+</head>
+
+ <body>
+
+ <div class="mainWrap">
+
+ 
+ <a id="touch-menu" class="mobile-menu" href="#"><i class="icon-reorder"></i>Menu</a>
+
+    <nav>
     <ul class="menu">
    <li><a href="#"><i class="icon-home"></i>HOME</a>
    <ul class="sub-menu">
@@ -26,3 +56,9 @@
   <li><a  href="#"><i class="icon-envelope-alt"></i>CONTACT</a></li>
   </ul>
   </nav>
+
+
+ </div><!--end mainWrap-->
+
+</body>
+</html>
