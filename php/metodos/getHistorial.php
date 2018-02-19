737bdbb89f82b913d2df5ff05c0813acd6d72278
Erick Cordero Rojas <eguicoro2@gmail.com>
2018-02-19 10:35:00 -0600

    GetHistorial con fecha completo

--- php/metodos/getHistorial.php
+++ php/metodos/getHistorial.php
@@ -13,14 +13,22 @@
   $stmt = $conexion->prepare($sql);
 
   //hay que cambiar el formato de las fechas prque se guardan de manera diferente a como se usan en Sql
-  $fecha1 = str_split($_COOKIE["FECHA1"], 2);
-  $fecha2 = str_split($_COOKIE["FECHA2"], 2);
+  //se quitan los -
+  $fecha1 = str_replace("-","",$_COOKIE["FECHA1"]);
+  $fecha2 = str_replace("-","",$_COOKIE["FECHA2"]);
+  //se separan los números
+  $fecha1 = str_split($fecha1,2);
+  $fecha2 = str_split($fecha2,2);
+  //formamos la fecha con el formato correto
+  $fecha1 = $fecha1[3]."/".$fecha1[2]."/".$fecha1[1];
+  $fecha2 = $fecha2[3]."/".$fecha2[2]."/".$fecha2[1];
+
   if ($stmt) {
     //se realiza un execute y un fetch donde se obtienen los datos de la primera fila
     //que coincida con el usuario y la clave ademas del cia.
     //en el execute se agregan las variables por medio de un array.
-    $stmt->execute(array($_COOKIE["COD_CLIENTE"], $_COOKIE["FECHA1"], $_COOKIE["FECHA2"]));
-    echo "<script>console.log( 'Debug Objects: " . $_COOKIE["FECHA1"]. "' );</script>";
+    $stmt->execute(array($_COOKIE["COD_CLIENTE"], $fecha1, $fecha2));
+    //echo "<script>console.log( 'Debug Objects: " . json_encode($fecha1). json_encode($fecha2). "' );</script>";
     while($fila = $stmt->fetch()){
       $row[] = $fila;
     }
