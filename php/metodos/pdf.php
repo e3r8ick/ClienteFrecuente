27a468f4066f06de6a098d2e7fe3540b42b5dfdc
Erick Cordero Rojas <eguicoro2@gmail.com>
2018-03-12 16:52:53 -0600

    Tabla fpdf

--- php/metodos/pdf.php
+++ php/metodos/pdf.php
@@ -6,7 +6,7 @@ class pdf extends FPDF
    function Header()
    {
 
-       $this->Image('../../images/lanco.png',75,8,60);
+      $this->Image('../../images/lanco.png',75,8,60);
 
       $this->SetFont('Arial','B',10);
 
@@ -19,12 +19,35 @@ class pdf extends FPDF
       $this->Cell(30,7,'02/03/2018',1,0,'C');
 
    }
-}
 
-//Creación del objeto de la clase heredada
-$pdf=new pdf();
-$pdf->AddPage();
-$pdf->SetFont('Times','',12);
-//Aquí escribimos lo que deseamos mostrar...
+   function TablaBasica($header)
+   {
+    //Cabecera
+    foreach($header as $col)
+    $this->Cell(40,7,$col,1);
+    $this->Ln();
+
+      $this->Cell(40,5,"hola",1);
+      $this->Cell(40,5,"hola2",1);
+      $this->Cell(40,5,"hola3",1);
+      $this->Cell(40,5,"hola4",1);
+      $this->Ln();
+      $this->Cell(40,5,"linea ",1);
+      $this->Cell(40,5,"linea 2",1);
+      $this->Cell(40,5,"linea 3",1);
+      $this->Cell(40,5,"linea 4",1);
+   }
+}
+   //Creación del objeto de la clase heredada
+    $pdf=new PDF();
+    //Títulos de las columnas
+    $header=array('Columna 1','Columna 2','Columna 3','Columna 4');
+    $pdf->AliasNbPages();
+    //Primera página
+    $pdf->AddPage();
+    $pdf->SetY(65);
+    //$pdf->AddPage();
+    $pdf->TablaBasica($header);
+
 $pdf->Output();
 ?>
